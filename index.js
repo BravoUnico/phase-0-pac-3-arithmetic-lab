98132dcbfbfe9f15460abdc21b6f59b595d9dc0f
Alejandro <sancha0701@gmail.com>
2022-05-26 18:38:25 -0400

    commit tree 7c211ef458f2aa2b1c0d706565a584fa4c216512

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+number = 10;
+function add5() {
+    return (number ++)
+}
+ 
+function increment(n) {
+    return (n + 1);
+}
+
+
+function decrement(a) {
+    return (a - 1);
+}
+
+function makeInt(a) {
+   return parseInt(a, 10);
+        
+}
+function preserveDecimal(a) {
+    return parseFloat(a, 10);
+}
